14f9c3187672f1458b5c8aac129a28af3cb9c652
Karl Carriere <carrierekarl@gmail.com>
2022-09-07 09:56:02 -0400

    Méthodes permettant de s'authentifier comme utilisateur implémentées dans la page Accueil

--- app/src/main/java/com/example/tp1_casino/MainActivity.java
+++ app/src/main/java/com/example/tp1_casino/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tp1_casino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -9,16 +10,27 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtNom;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sp = getSharedPreferences("Utilisateurs", MODE_PRIVATE);
+
         txtNom = findViewById(R.id.txtNom);
     }
 
     public void gererClick(View v){
 
+        SharedPreferences.Editor editor = sp.edit();
+        String stringNom = txtNom.toString();
+        if (!sp.contains(stringNom)){
+            editor.putInt(stringNom, 15);
+        }
+
+        editor.putString("session", stringNom);
+        editor.apply();
     }
 }
